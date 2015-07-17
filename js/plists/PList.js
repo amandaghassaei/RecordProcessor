4908e496e8737845bc24f4e464358646328dd6e7
Amanda Ghassaei <amandaghassaei@gmail.com>
2015-07-18 00:26:09 +0400

    first commit

---
+++ js/plists/PList.js
@@ -0,0 +1,15 @@
+//all property lists for the app, these are "static" variables
+
+
+define(['three'], function(THREE){
+
+    return {
+
+        allMenuTabs: {
+            navDesign:{
+                default:"Default"
+            }
+        }
+
+    }
+});
